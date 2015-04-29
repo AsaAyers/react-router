e360e32636e950ed88b610eb8beaa2ffeb75dc54
Asa Ayers <Asa@AsaAyers.com>
2015-04-29 12:52:44 -0400

    Disable NODE_ENV replacement

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 var plugins = [
   new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    'process.env.NODE_ENVX': JSON.stringify(process.env.NODE_ENV)
   })
 ];
 
